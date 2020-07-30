d41548980dfd81610d2d6a04f53606b53ccecee6
Wojtek Nowak <billnewmann@gmail.com>
2020-07-30 21:25:42 +0100

    Add 3rd test and fix cutText

--- utils/cutText.js
+++ utils/cutText.js
@@ -1,5 +1,6 @@
 module.exports = (content, maxLength) => {
   if(typeof content !== 'string') return 'Error';
+  if(typeof maxLength !== 'number') return 'Error';
   if(content.length < 1) return 'Error';
   if(content.length <= maxLength) return content;
   return content.substr(0, content.lastIndexOf(' ', maxLength)) + '...';
--- utils/test/cutText.test.js
+++ utils/test/cutText.test.js
@@ -14,5 +14,13 @@ describe('CutText', () => {
       it('should return an error if "content" arg length is 0', () => {
         expect(cutText('', 20)).to.equal('Error');
       });
+
+      it('should return an error if "maxLength" arg is not a number', () => {
+        expect(cutText('Lorem Ipsum', undefined)).to.equal('Error');
+        expect(cutText('Lorem Ipsum', 'abc')).to.equal('Error');
+        expect(cutText('Lorem Ipsum', {})).to.equal('Error');
+        expect(cutText('Lorem Ipsum', [])).to.equal('Error');
+        expect(cutText('Lorem Ipsum', function() {})).to.equal('Error');
+      });
   
   });
