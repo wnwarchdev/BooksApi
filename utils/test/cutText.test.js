4386ad7ed27824b73c18cf39a539d6477e8b07ce
Wojtek Nowak <billnewmann@gmail.com>
2020-07-30 21:41:45 +0100

    Add final test

--- utils/test/cutText.test.js
+++ utils/test/cutText.test.js
@@ -32,5 +32,11 @@ describe('CutText', () => {
         expect(cutText('Lorem Ipsum', 40)).to.equal('Lorem Ipsum');
         expect(cutText('Lorem Ipsum', 11)).to.equal('Lorem Ipsum');
       });
+
+      it('should return good cut "content" if proper args', () => {
+        expect(cutText('Lorem Ipsum dolor sit amet', 14)).to.equal('Lorem Ipsum...');
+        expect(cutText('Lorem Ipsum dolor sit amet', 5)).to.equal('Lorem...');
+        expect(cutText('Lorem Ipsum dolor sit amet', 17)).to.equal('Lorem Ipsum dolor...');
+      });
   
   });
