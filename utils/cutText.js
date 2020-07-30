6cd10b25b2cda3f025680c639b037b47cf21fd8e
Wojtek Nowak <billnewmann@gmail.com>
2020-07-30 21:22:44 +0100

    Fix cutText

--- utils/cutText.js
+++ utils/cutText.js
@@ -1,4 +1,5 @@
 module.exports = (content, maxLength) => {
+  if(typeof content !== 'string') return 'Error';
   if(content.length < 1) return 'Error';
   if(content.length <= maxLength) return content;
   return content.substr(0, content.lastIndexOf(' ', maxLength)) + '...';
