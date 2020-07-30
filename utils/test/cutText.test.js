f5d997ce6d9d11aa83a23c340e9d849b663634a5
Wojtek Nowak <billnewmann@gmail.com>
2020-07-30 21:24:18 +0100

    Add 2nd test

--- utils/test/cutText.test.js
+++ utils/test/cutText.test.js
@@ -10,5 +10,9 @@ describe('CutText', () => {
         expect(cutText([], 20)).to.equal('Error');
         expect(cutText(function() {}, 20)).to.equal('Error');
       });
+
+      it('should return an error if "content" arg length is 0', () => {
+        expect(cutText('', 20)).to.equal('Error');
+      });
   
   });
